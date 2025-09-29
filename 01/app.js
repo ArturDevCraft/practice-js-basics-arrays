b740f75e95a828b2db1312b0669dfa698c5e5aec
Artur <artur.czerwiak@gmail.com>
2025-09-29 08:54:00 +0200

    Task 01 completed

---
+++ 01/app.js
@@ -0,0 +1,9 @@
+let users = [
+	'Jan Kowalski',
+	'Grzegorz Małolepszy',
+	'Adam Nieproszony',
+	'Katarzyna Nowak',
+	'Olga Zarzeczna',
+];
+
+console.log(users[0], users[2], users[4], users.length);
